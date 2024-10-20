f6193cb0cec25b09547a7043bbba36eb94523cfa
javad <javadbayati48@gmail.com>
2024-10-20 18:17:40 +0330

    first commit

---
+++ list.js
@@ -0,0 +1,16 @@
+
+
+
+const myList = [1, 11, 22, 23, 34, 45, 65];
+
+
+
+let sum=0;
+
+    for (let i = 0; i < myList.length; i++) {
+        sum += myList[i]; 
+
+  
+
+    }
+    console.log("sum =" + sum)
